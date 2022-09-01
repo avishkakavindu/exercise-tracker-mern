2a60bd0a50254490adf4cf470b6eaf73abe27b7d
avishkakavindu <avishkakavindud@gmail.com>
2022-09-01 17:53:48 +0530

    DB connection established

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -7,9 +8,22 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// middleware
+// Middleware
 app.use(cors);
 app.use(express.json()); // allow to parse json data
+
+// Database settings
+const DB_URI = process.env.DB_URI;
+
+mongoose.connect(DB_URI, (err) => {
+  console.log('Mongo DB connection', err);
+});
+
+const connection = mongoose.connection;
+// once the connection is open then
+connection.once('open', () => {
+  console.log('Connected to database successfully!');
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}...`);
